4308a1e1edcc61c921b54774c2fac42e6f7d4715
Michael P Mariani PhD <m.mariani123@gmail.com>
2022-11-04 15:29:16 -0400

    update working on dog 11042022
    update working on dog

--- R/A_ExonPtnAlign_functions_dog.R
+++ R/A_ExonPtnAlign_functions_dog.R
@@ -3,25 +3,45 @@
 #' Download Protein Alignment and Accessory Files
 #' @param Loci HLA Loci to be fetched. Limited Loci available.
 #' @note This function is for internal BIGDAWG use only.
-GetFiles <- function(Loci) {
+GetFiles <- function(Loci){
   #downloads *_prot.txt alignment files
   #downloads hla_nom_p.txt file
 
   # Get P-Groups Files
-  download.file("https://raw.githubusercontent.com/ANHIG/IMGTHLA/Latest/wmda/hla_nom_p.txt",destfile="hla_nom_p.txt",method="libcurl")
+  #download.file(
+  #  "https://raw.githubusercontent.com/ANHIG/IMGTHLA/Latest/wmda/hla_nom_p.txt",
+  #  destfile="hla_nom_p.txt",
+  #  method="libcurl")
 
   # Get Release Version
-  df <- readLines(con="hla_nom_p.txt", n=3)
+  df <- readLines(con=paste0("C:/Users/Mike_2/Documents",
+                             "/GitHub/BIGDAWGv2/canid_DQB1_prot.txt"),
+        n=3)
+
+  df <- readLines(con="canid_DQB1_prot.txt", n=3)
+  #df <- readLines(con="dla_nom_p.txt", n=3)
   RD <- unlist(strsplit(df[2],split=" "))[3]
-  RV <- paste(unlist(strsplit(df[3],split=" "))[3:4],collapse=" ")
-  write.table(c(RD,RV),file="Release.txt",quote=F,col.names=F,row.names=F)
+  #RV <- paste(unlist(strsplit(df[3], split=" "))[3:4],collapse=" ")
+  RV <- df[3]
+  write.table(c(RD,RV),
+              file="Dog_Release.txt",
+              quote=F,
+              col.names=F,
+              row.names=F)
 
   # Get Locus Based Alignments
-  for(i in 1:length(Loci)) {
+  for(i in 1:length(Loci)){
     Locus <- Loci[i]
-    FileName <- paste(Locus,"_prot.txt",sep="")
-    URL <- paste("https://raw.githubusercontent.com/ANHIG/IMGTHLA/Latest/alignments/",FileName,sep="")
-    download.file(URL,destfile = FileName,method="libcurl")
+    FileName <- paste0(Locus,"_prot.txt")
+    URL <- paste0(
+      "https://raw.githubusercontent.com/mmariani123/BIGDAWGv2/latest/alignments",
+      FileName)
+    #"https://raw.githubusercontent.com/ANHIG/IMGTHLA/Latest/alignments is 404"
+    #But individual files are found, e.g.:
+    #https://raw.githubusercontent.com/ANHIG/IMGTHLA/Latest/alignments/B_prot.txt
+    download.file(URL,
+                  destfile = FileName,
+                  method="libcurl")
   }
 
 }
@@ -32,23 +52,35 @@ GetFiles <- function(Loci) {
 #' @param x P group object from read.table command.
 #' @param Locus Locus to be filtered on.
 #' @note This function is for internal BIGDAWG use only.
-PgrpFormat <- function(x,Locus) {
+PgrpFormat_dog <- function(x,Locus){
 
   # Identify for Locus ... change necessary if DRB
   x.sub <- x[which(x[,1]==Locus),]
   rownames(x.sub) <- NULL
-  x.sub[,2] <- sapply(x.sub[,2],function(i) paste(paste(Locus,"*",unlist(strsplit(i,"/")),sep=""),collapse="/"))
+  x.sub[,2] <- sapply(x.sub[,2],
+                      function(i){
+                        paste(
+                          paste0(Locus,
+                                 "*",
+                                 unlist(strsplit(i,"/"))),
+                          collapse="/")
+                      }
+  )
   colnames(x.sub) <- c("Locus","Allele","P.Group")
 
   #Expand
   x.list <- list()
   for(i in 1:nrow(x.sub)) {
-    if(grepl("/",x.sub[i,'Allele'],fixed=T)) {
+    if(grepl("/",x.sub[i,'Allele'],fixed=T)){
       tmp <- unlist(strsplit(x.sub[i,'Allele'],"/"))
-      tmp <- cbind(rep(Locus,length(tmp)),tmp,rep(x.sub[i,'P.Group'],length(tmp)))
+      tmp <- cbind(rep(Locus,length(tmp)),
+                   tmp,
+                   rep(x.sub[i,'P.Group'],
+                       length(tmp))
+      )
       colnames(tmp) <- colnames(x.sub)
       x.list[[i]] <- tmp
-    } else {
+    }else{
       x.list[[i]] <- x.sub[i,]
     }
   }
@@ -67,10 +99,15 @@ PgrpFormat <- function(x,Locus) {
 #' @note This function is for internal BIGDAWG use only.
 PgrpExtract <- function(x,y) {
   getRow <- grep(x,y[,'Allele'],fixed=T)
-  if(length(getRow)>=1) {
-    if(length(getRow)>1) { getRow <- getRow[which(sapply(as.character(y[getRow,'Allele']),nchar)==nchar(x))] }
+  if(length(getRow)>=1){
+    if(length(getRow)>1){
+      getRow <- getRow[which(sapply(as.character(y[getRow,'Allele']),
+                                    nchar)==nchar(x))]
+    }
     return(as.character(y[getRow,'P.Group']))
-  } else { return("") }
+  }else{
+    return("")
+  }
 }
 
 #' Protein Exon Alignment Formatter
@@ -79,23 +116,39 @@ PgrpExtract <- function(x,y) {
 #' @param Locus Locus alignment to be formatted.
 #' @param RefTab Reference exon protein information for alignment formatting.
 #' @note This function is for internal BIGDAWG use only.
-ExonPtnAlign.Create <- function(Locus,RefTab) {
+ExonPtnAlign.Create <- function(Locus,RefTab){
 
   #########################################################################
   # Need to remove if DRB split into single locus files
-  if(grepl("DRB",Locus)) { Locus.get <- "DRB" } else { Locus.get <- Locus }
+  if(grepl("DRB",Locus)){
+    Locus.get <- "DRB"
+  }else{
+    Locus.get <- Locus
+  }
   #########################################################################
 
   AlignMatrix <- NULL; rm(AlignMatrix)
 
   #Read in P-Groups
-  Pgrps <- read.table("hla_nom_p.txt",fill=T,header=F,sep=";",stringsAsFactors=F,strip.white=T,colClasses="character")
-  Pgrps[,1] <- gsub("\\*","",Pgrps[,1])
+  Pgrps <- read.table("hla_nom_p.txt",
+                      fill=T,
+                      header=F,
+                      sep=";",
+                      stringsAsFactors=F,
+                      strip.white=T,
+                      colClasses="character")
+  Pgrps[,1] <- gsub("\\*", "", Pgrps[,1])
   Pgrps <- PgrpFormat(Pgrps,Locus)
 
   #Read in Alignment
   Name <- paste0(Locus.get,"_prot.txt")
-  Align <- read.table(Name,fill=T,header=F,sep="\t",stringsAsFactors=F,strip.white=T,colClasses="character")
+  Align <- read.table(Name,
+                      fill=T,
+                      header=F,
+                      sep="\t",
+                      stringsAsFactors=F,
+                      strip.white=T,
+                      colClasses="character")
 
   #Trim
   Align <- as.matrix(Align[-nrow(Align),]) #Remove Footer
@@ -129,19 +182,27 @@ ExonPtnAlign.Create <- function(Locus,RefTab) {
   for( i in 1:nrow(Block) ) {
     Block.miss <- Block.len - nchar(Block[i,2])
     if( Block.miss > 0 ) {
-      Block[i,2] <- paste0(as.character(Block[i,2]), paste(rep(".",Block.miss),collapse=""))
+      Block[i,2] <- paste0(as.character(Block[i,2]),
+                           paste(rep(".",Block.miss),collapse=""))
     }
   }; rm(i)
 
   #Split Allele name into separate Locus and Allele, Send Back to Align object
   AlignAlleles <- do.call(rbind,strsplit(Block[,1],"[*]"))
-  AlignAlleles <- cbind(AlignAlleles,apply(AlignAlleles,MARGIN=c(1,2),FUN=GetField,Res=2)[,2])
+  AlignAlleles <-
+    cbind(AlignAlleles,
+          apply(AlignAlleles,MARGIN=c(1,2),FUN=GetField,Res=2)[,2])
   rownames(AlignAlleles) <- NULL
 
   Align <- cbind(AlignAlleles,Block)
-  colnames(Align) <- c("Locus","Allele","Trimmed","FullName","Sequence")
+  colnames(Align) <- c("Locus",
+                       "Allele",
+                       "Trimmed",
+                       "FullName",
+                       "Sequence")
 
-  #Split Sub Alignment into composite elements and Extract relevant positions
+  #Split Sub Alignment into composite elements and
+  #Extract relevant positions
   Align.split <- strsplit(Align[,'Sequence'],"")
   Align.split <- do.call(rbind,Align.split)
   AlignMatrix <- cbind(Align[,1:4],Align.split)
@@ -151,10 +212,12 @@ ExonPtnAlign.Create <- function(Locus,RefTab) {
   RefAllele <- paste(RefTab[which(RefTab[,'Locus']==Locus),'Reference.Locus'],
                      RefTab[which(RefTab[,'Locus']==Locus),'Reference.Allele'],
                      sep="*")
-  if( !AlignMatrix[1,'FullName']==RefAllele ) {
+  if(!AlignMatrix[1,'FullName']==RefAllele){
 
-    Align.tmp <- rbind(AlignMatrix[which(AlignMatrix[1,'Allele']==RefAllele),],
-                       AlignMatrix[-which(AlignMatrix[1,'Allele']==RefAllele),])
+    Align.tmp <- rbind(AlignMatrix[
+      which(AlignMatrix[1,'Allele']==RefAllele),],
+      AlignMatrix[
+        -which(AlignMatrix[1,'Allele']==RefAllele),])
     AlignMatrix <- Align.tmp
     rm(Align.tmp)
 
@@ -167,15 +230,27 @@ ExonPtnAlign.Create <- function(Locus,RefTab) {
   AlignMatrix <- AlignMatrix[which(AlignMatrix[,'Locus']==Locus),]
 
   #Rebind Reference if removed (only for DRB3, DRB4, and DRB5)
-  if( !AlignMatrix[1,'FullName']==RefAllele ) { AlignMatrix <- rbind(RefSeq,AlignMatrix) }
+  if(!AlignMatrix[1,'FullName']==RefAllele){
+    AlignMatrix <- rbind(RefSeq,AlignMatrix)
+  }
 
-  #Remove columns with no amino acids positions (only for DRB3, DRB4, and DRB5)
-  #Count occurence of "." and compare to nrow of AlignMatrix
-  rmCol <- which( apply(AlignMatrix,MARGIN=2, FUN=function(x) length(which(x=="."))) == nrow(AlignMatrix) )
-  if( length(rmCol)>0 ) { AlignMatrix <- AlignMatrix[,-rmCol] }
+  #Remove columns with no amino acids positions
+  #(only for DRB3, DRB4, and DRB5)
+  #Count occurence of "." and compare to nrow
+  #of AlignMatrix
+  rmCol <- which(apply(AlignMatrix,
+                       MARGIN=2,
+                       {FUN=function(x){
+                         length(which(x=="."))
+                       }}) == nrow(AlignMatrix)
+  )
+
+  if(length(rmCol)>0){
+    AlignMatrix <- AlignMatrix[,-rmCol]
+  }
 
   #Propagate Consensus Positions
-  for( i in 5:ncol(AlignMatrix) ) {
+  for(i in 5:ncol(AlignMatrix)){
     x <- AlignMatrix[,i]
     x[which(x=="-")] <- x[1]
     AlignMatrix[,i] <- x
@@ -183,46 +258,76 @@ ExonPtnAlign.Create <- function(Locus,RefTab) {
 
   #Rename amino acid positions based on reference numbering
   #Deletions are named according to the preceding position with a .1,.2,etc.
-  RefStart <- as.numeric(RefTab[which(RefTab[,'Locus']==Locus),'Reference.Start'])
+  RefStart <- as.numeric(RefTab[which(RefTab[,'Locus']==Locus),
+                                'Reference.Start'])
   RefArray <- AlignMatrix[1,5:ncol(AlignMatrix)]
   Names <- NULL ; RefPos <- RefStart
   for(i in 1:length(RefArray) ) {
 
     if(RefArray[i]==".") {
-      Names <- c(Names, paste0("Pos.",RefPos-1,".",Iteration) )
+      Names <- c(Names,
+                 paste0("Pos.",
+                        RefPos-1,
+                        ".",
+                        Iteration) )
       Iteration = Iteration + 1
-    } else {
+    }else{
       Iteration=1
       Names <- c(Names, paste0("Pos.",RefPos))
       RefPos <- RefPos + 1
-      if (RefPos==0) { RefPos <- 1 }
+      if(RefPos==0){RefPos <- 1}
     }
 
   }
   colnames(AlignMatrix)[5:ncol(AlignMatrix)] <- Names
   rownames(AlignMatrix) <- NULL
 
-  #Add Absent Allele (Absence due to lack of allele and not lack of typing information)
-  AlignMatrix <- rbind(c(Locus,"00:00:00:00","00:00",paste(Locus,"*00:00:00:00",sep=""),rep("^",ncol(AlignMatrix)-4)),
+  #Add Absent Allele (Absence due to lack of allele
+  #and not lack of typing information)
+  AlignMatrix <- rbind(c(Locus,
+                         "00:00:00:00",
+                         "00:00",
+                         paste0(Locus,"*00:00:00:00"),
+                         rep("^",ncol(AlignMatrix)-4)),
                        AlignMatrix)
 
   #Assign P groups
-  AlignMatrix <- cbind(AlignMatrix, sapply(AlignMatrix[,'FullName'],PgrpExtract,y=Pgrps) )
+  AlignMatrix <- cbind(AlignMatrix,
+                       sapply(AlignMatrix[,'FullName'],
+                              PgrpExtract,y=Pgrps) )
+
   colnames(AlignMatrix)[ncol(AlignMatrix)] <- "P.group"
 
   #Tally Unknowns as separate column
   AlignMatrix <- cbind(AlignMatrix,
-                       apply(AlignMatrix[,5:(ncol(AlignMatrix)-1)],MARGIN=1,FUN=function(x) length(which(unlist(grep("*",x,fixed=T))>0))) )
+                       apply(AlignMatrix[,5:(ncol(AlignMatrix)-1)],
+                             MARGIN=1,
+                             FUN=function(x){length(which(unlist(grep("*",x,fixed=T))>0))}
+                       )
+  )
+
   colnames(AlignMatrix)[ncol(AlignMatrix)] <- "Unknowns"
 
   #Tally Null Positions as separate column
   AlignMatrix <- cbind(AlignMatrix,
-                       apply(AlignMatrix[,5:(ncol(AlignMatrix)-2)],MARGIN=1,FUN=function(x) length(which(unlist(grep("-",x,fixed=T))>0))) )
+                       apply(AlignMatrix[,5:(ncol(AlignMatrix)-2)],
+                             MARGIN=1,
+                             FUN=function(x){
+                               length(which(unlist(grep("-",x,fixed=T))>0))}
+                       )
+  )
+
   colnames(AlignMatrix)[ncol(AlignMatrix)] <- "NullPositions"
 
   #Tally InDels
   AlignMatrix <- cbind(AlignMatrix,
-                       apply(AlignMatrix[,5:(ncol(AlignMatrix)-3)],MARGIN=1,FUN=function(x) length(which(unlist(grep(".",x,fixed=T))>0))) )
+                       apply(AlignMatrix[,5:(ncol(AlignMatrix)-3)],
+                             MARGIN=1,
+                             FUN=function(x){
+                               length(which(unlist(grep(".",x,fixed=T))>0))}
+                       )
+  )
+
   colnames(AlignMatrix)[ncol(AlignMatrix)] <- "InDels"
 
   rownames(AlignMatrix) <- NULL
@@ -265,14 +370,14 @@ AlignObj.Create <- function(Loci,Release,RefTab) {
 #' @param Release IMGT/HLA database release version.
 #' @param RefTab Data of reference exons used for protein alignment creation.
 #' @note This function is for internal BIGDAWG use only.
-AlignObj.Update <- function(Loci,Release,RefTab) {
+AlignObj.Update <- function(Loci,Release,RefTab){
 
   AlignMatrix <- NULL; rm(AlignMatrix)
 
   UpdatePtnList <- list()
-  for(i in 1:length(Loci)) {
+  for(i in 1:length(Loci)){
     Locus <- Loci[i]
-    FileName <- paste("ExonPtnAlign_",Locus,".obj",sep="")
+    FileName <- paste0("ExonPtnAlign_",Locus,".obj")
     load(FileName) #Loads AlignMatrix
     UpdatePtnList[[Locus]] <- AlignMatrix
   }
